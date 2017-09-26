2acdca57aa8144d0768cc899f34e95bf089eddf1
DenisMishin <denis@gravitational.io>
2017-09-26 18:03:43 +0000

    local checks

---
+++ monitoring/local.go
@@ -0,0 +1,15 @@
+package monitoring
+
+import (
+	"github.com/gravitational/satellite/agent/health"
+)
+
+// LocalCheckers
+func LocalCheckers() health.Checker {
+	return &compositeChecker{
+		name: "local",
+		checkers: []health.Checker{
+			&IPForwardChecker{},
+		},
+	}
+}
