d7671ae3d8d86a7a4d31875f4cb3f02a966d6b26
Bernard Kim <bernard@gravitational.com>
2020-01-15 15:46:03 -0800

    Refactor agent.go
    - Config has nested SerfConfig and TimelineConfig.
    - agent has embedded mutex.
    - agent uses single clock.
    - separate recycle and status update loop.

--- agent/agent.go
+++ agent/agent.go
@@ -85,8 +85,8 @@ type Config struct {
 	// KeyFile specifies the path to TLS certificate key file
 	KeyFile string
 
-	// RPC address of local serf node.
-	SerfRPCAddr string
+	// SerfConfig specifies serf client configuration.
+	SerfConfig serf.Config
 
 	// Address to listen on for web interface and telemetry for Prometheus metrics.
 	MetricsAddr string
@@ -100,11 +100,8 @@ type Config struct {
 	// Tags is a trivial means for adding extra semantic information to an agent.
 	Tags map[string]string
 
-	// DBPath specifies the location of the database to be used to store timeline.
-	DBPath string
-
-	// RetentionDuration specifies the duration to store timeline events.
-	RetentionDuration time.Duration
+	// TimelineConfig specifies sqlite timeline configuration.
+	TimelineConfig sqlite.Config
 
 	// Clock to be used for internal time keeping.
 	Clock clockwork.Clock
@@ -113,125 +110,44 @@ type Config struct {
 	cache.Cache
 }
 
-// New creates an instance of an agent based on configuration options given in config.
-func New(config *Config) (Agent, error) {
-	if err := config.CheckAndSetDefaults(); err != nil {
-		return nil, trace.Wrap(err)
-	}
-
-	clientConfig := serf.Config{
-		Addr: config.SerfRPCAddr,
-	}
-	client, err := NewSerfClient(clientConfig)
-	if err != nil {
-		return nil, trace.Wrap(err, "failed to connect to serf")
-	}
-
-	err = client.UpdateTags(config.Tags, nil)
-	if err != nil {
-		return nil, trace.Wrap(err, "failed to update serf agent tags")
-	}
-
-	metricsListener, err := net.Listen("tcp", config.MetricsAddr)
-	if err != nil {
-		return nil, trace.Wrap(err, "failed to serve prometheus metrics")
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), timelineInitTimeout)
-	defer cancel()
-
-	sqliteConfig := sqlite.Config{
-		DBPath:            config.DBPath,
-		RetentionDuration: config.RetentionDuration,
-		Clock:             config.Clock,
-	}
-	timeline, err := sqlite.NewTimeline(ctx, sqliteConfig)
-	if err != nil {
-		return nil, trace.Wrap(err, "failed to initialize timeline")
-	}
-
-	agent := &agent{
-		Config:                  *config,
-		SerfClient:              client,
-		Timeline:                timeline,
-		name:                    config.Name,
-		cache:                   config.Cache,
-		dialRPC:                 DefaultDialRPC(config.CAFile, config.CertFile, config.KeyFile),
-		statusClock:             config.Clock,
-		recycleClock:            config.Clock,
-		timelineClock:           config.Clock,
-		statusQueryReplyTimeout: statusQueryReplyTimeout,
-		localStatus:             emptyNodeStatus(config.Name),
-		metricsListener:         metricsListener,
-		done:                    make(chan struct{}),
-	}
-
-	agent.rpc, err = newRPCServer(agent, config.CAFile, config.CertFile, config.KeyFile, config.RPCAddrs)
-	if err != nil {
-		return nil, trace.Wrap(err, "failed to create RPC server")
-	}
-	return agent, nil
-}
-
 // CheckAndSetDefaults validates this configuration object.
 // Config values that were not specified will be set to their default values if
 // available.
 func (r *Config) CheckAndSetDefaults() error {
 	var errors []error
-
 	if r.CAFile == "" {
 		errors = append(errors, trace.BadParameter("certificate authority file must be provided"))
 	}
-
 	if r.CertFile == "" {
 		errors = append(errors, trace.BadParameter("certificate must be provided"))
 	}
-
 	if r.KeyFile == "" {
 		errors = append(errors, trace.BadParameter("certificate key must be provided"))
 	}
-
 	if r.Name == "" {
 		errors = append(errors, trace.BadParameter("agent name cannot be empty"))
 	}
-
 	if len(r.RPCAddrs) == 0 {
 		errors = append(errors, trace.BadParameter("at least one RPC address must be provided"))
 	}
-
-	if r.DBPath == "" {
-		errors = append(errors, trace.BadParameter("sqlite database path must be provided"))
-	}
-
 	if r.Tags == nil {
 		r.Tags = make(map[string]string)
 	}
-
 	if r.Clock == nil {
 		r.Clock = clockwork.NewRealClock()
 	}
-
 	return trace.NewAggregate(errors...)
 }
 
 type agent struct {
+	sync.Mutex
 	health.Checkers
 
 	metricsListener net.Listener
 
-	// SerfClient provides access to the serf agent.
-	SerfClient SerfClient
-
-	// Name of this agent.  Must be the same as the serf agent's name
-	// running on the same node.
-	name string
-
 	// RPC server used by agent for client communication as well as
 	// status sync with other agents.
 	rpc RPCServer
-
-	// cache persists node status history.
-	cache cache.Cache
 
 	// dialRPC is a factory function to create clients to other agents.
 	// If future, agent address discovery will happen through serf.
@@ -240,16 +156,6 @@ type agent struct {
 	// done is a channel used for cleanup.
 	done chan struct{}
 
-	// TODO: do we need separate clocks here?
-	// Remove when refactoring. Just use Config.Clock.
-
-	// These clocks abstract away access to the time package to allow
-	// testing.
-	statusClock   clockwork.Clock
-	recycleClock  clockwork.Clock
-	timelineClock clockwork.Clock
-
-	mu sync.Mutex
 	// localStatus is the last obtained local node status.
 	localStatus *pb.NodeStatus
 
@@ -261,12 +167,73 @@ type agent struct {
 	// Config is the agent configuration.
 	Config
 
+	// SerfClient provides access to the serf agent.
+	SerfClient SerfClient
+
 	// Timeline keeps track of status change events.
 	Timeline history.Timeline
 }
 
-// DialRPC returns RPC client for the provided Serf member.
-type DialRPC func(context.Context, *serf.Member) (*client, error)
+// New creates an instance of an agent based on configuration options given in config.
+func New(config Config) (Agent, error) {
+	if err := config.CheckAndSetDefaults(); err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	client, err := initSerfClient(config.SerfConfig, config.Tags)
+	if err != nil {
+		return nil, trace.Wrap(err, "failed to initialize serf client")
+	}
+
+	// TODO: do we need to initialize metrics listener in constructor?
+	// Move to Start?
+	metricsListener, err := net.Listen("tcp", config.MetricsAddr)
+	if err != nil {
+		return nil, trace.Wrap(err, "failed to serve prometheus metrics")
+	}
+
+	timeline, err := initTimeline(config.TimelineConfig)
+	if err != nil {
+		return nil, trace.Wrap(err, "failed to initialize timeline")
+	}
+
+	agent := &agent{
+		dialRPC:                 DefaultDialRPC(config.CAFile, config.CertFile, config.KeyFile),
+		statusQueryReplyTimeout: statusQueryReplyTimeout,
+		localStatus:             emptyNodeStatus(config.Name),
+		metricsListener:         metricsListener,
+		done:                    make(chan struct{}),
+		Config:                  config,
+		SerfClient:              client,
+		Timeline:                timeline,
+	}
+
+	agent.rpc, err = newRPCServer(agent, config.CAFile, config.CertFile, config.KeyFile, config.RPCAddrs)
+	if err != nil {
+		return nil, trace.Wrap(err, "failed to create RPC server")
+	}
+	return agent, nil
+}
+
+// initSerfClient initializes a new serf client and modifies the client with
+// the provided tags.
+func initSerfClient(config serf.Config, tags map[string]string) (SerfClient, error) {
+	client, err := NewSerfClient(config)
+	if err != nil {
+		return nil, trace.Wrap(err, "failed to connect to serf")
+	}
+	if err = client.UpdateTags(tags, nil); err != nil {
+		return nil, trace.Wrap(err, "failed to update serf agent tags")
+	}
+	return client, nil
+}
+
+// initTimeline initializes a new sqlite timeline.
+func initTimeline(config sqlite.Config) (history.Timeline, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timelineInitTimeout)
+	defer cancel()
+	return sqlite.NewTimeline(ctx, config)
+}
 
 // GetConfig returns the agent configuration.
 func (r *agent) GetConfig() Config {
@@ -275,25 +242,29 @@ func (r *agent) GetConfig() Config {
 
 // Start starts the agent's background tasks.
 func (r *agent) Start() error {
-	errChan := make(chan error, 1)
+	// TODO: Modify Start to accept a context.
+	ctx := context.TODO()
+	go r.recycleLoop(ctx)
+	go r.statusUpdateLoop(ctx)
+	go r.timelineUpdateLoop(ctx)
+	go r.serveMetrics(ctx)
+	return nil
+}
 
-	go r.statusUpdateLoop()
-	go r.timelineUpdateLoop(context.TODO())
-
-	if r.metricsListener != nil {
-		go func() {
-			http.Handle("/metrics", promhttp.Handler())
-			if err := http.Serve(r.metricsListener, nil); err != nil {
-				errChan <- trace.Wrap(err)
-			}
-		}()
+// serveMetrics registers the prometheus metrics handler and starts accepting
+// connections on the metrics listener.
+func (r *agent) serveMetrics(ctx context.Context) {
+	if r.metricsListener == nil {
+		return
 	}
 
-	select {
-	case err := <-errChan:
-		return err
-	case <-time.After(1 * time.Second):
-		return nil
+	http.Handle("/metrics", promhttp.Handler())
+	err := http.Serve(r.metricsListener, nil)
+	if err == http.ErrServerClosed {
+		log.WithError(err).Debug("Metrics listener has been shutdown/closed.")
+	}
+	if err != nil {
+		log.WithError(err).Errorf("Failed to server metrics.")
 	}
 }
 
@@ -306,11 +277,11 @@ func (r *agent) IsMember() bool {
 	}
 	// if we're the only one, consider that we're not in the cluster yet
 	// (cause more often than not there are more than 1 member)
-	if len(members) == 1 && members[0].Name == r.name {
+	if len(members) == 1 && members[0].Name == r.Name {
 		return false
 	}
 	for _, member := range members {
-		if member.Name == r.name {
+		if member.Name == r.Name {
 			return true
 		}
 	}
@@ -373,7 +344,7 @@ func (r *agent) runChecks(ctx context.Context) *pb.NodeStatus {
 			go runChecker(ctxChecks, c, probeCh, semaphoreCh)
 		case <-ctx.Done():
 			log.Warnf("Timed out running tests: %v.", ctx.Err())
-			return emptyNodeStatus(r.name)
+			return emptyNodeStatus(r.Name)
 		}
 	}
 
@@ -385,7 +356,7 @@ func (r *agent) runChecks(ctx context.Context) *pb.NodeStatus {
 		case <-ctx.Done():
 			log.Warnf("Timed out collecting test results: %v.", ctx.Err())
 			return &pb.NodeStatus{
-				Name:   r.name,
+				Name:   r.Name,
 				Status: pb.NodeStatus_Degraded,
 				Probes: probes.GetProbes(),
 			}
@@ -393,7 +364,7 @@ func (r *agent) runChecks(ctx context.Context) *pb.NodeStatus {
 	}
 
 	return &pb.NodeStatus{
-		Name:   r.name,
+		Name:   r.Name,
 		Status: probes.Status(),
 		Probes: probes.GetProbes(),
 	}
@@ -445,51 +416,46 @@ func runChecker(ctx context.Context, checker health.Checker, probeCh chan<- heal
 	}
 }
 
+// recycleLoop periodically recycles the cache.
+func (r *agent) recycleLoop(ctx context.Context) {
+	ticker := r.Clock.NewTicker(recycleTimeout)
+	defer ticker.Stop()
+	for range ticker.Chan() {
+		if utils.IsContextDone(ctx) {
+			log.Info("Recycle loop is stopping.")
+			return
+		}
+		if err := r.Cache.Recycle(); err != nil {
+			log.WithError(err).Warnf("Error recycling status.")
+			continue
+		}
+	}
+}
+
 // statusUpdateLoop is a long running background process that periodically
 // updates the health status of the cluster by querying status of other active
 // cluster members.
-func (r *agent) statusUpdateLoop() {
-	statusUpdateCh := r.statusClock.After(statusUpdateTimeout)
-	recycleCh := r.recycleClock.After(recycleTimeout)
-
-	// collectCh is a channel that receives updates when the status collecting process
-	// has finished collecting
-	collectCh := make(chan struct{})
-	for {
-		select {
-		case <-statusUpdateCh:
-			ctx, cancel := context.WithTimeout(context.Background(), r.statusQueryReplyTimeout)
-			go func() {
-				defer cancel() // close context if collection finishes before the deadline
-				status, err := r.collectStatus(ctx)
-				if err != nil {
-					log.Warnf("Error collecting system status: %v.", err)
-				}
-				if status != nil {
-					if err = r.cache.UpdateStatus(status); err != nil {
-						log.Warnf("Error updating system status in cache: %v", err)
-					}
-				}
-				select {
-				case <-r.done:
-				case collectCh <- struct{}{}:
-				}
-			}()
-			select {
-			case <-collectCh:
-				statusUpdateCh = r.statusClock.After(statusUpdateTimeout)
-			case <-r.done:
-				cancel()
-				return
-			}
-		case <-recycleCh:
-			err := r.cache.Recycle()
-			if err != nil {
-				log.Warningf("error recycling stats: %v", err)
-			}
-			recycleCh = r.recycleClock.After(recycleTimeout)
-		case <-r.done:
+func (r *agent) statusUpdateLoop(ctx context.Context) {
+	ticker := r.Clock.NewTicker(statusUpdateTimeout)
+	defer ticker.Stop()
+	for range ticker.Chan() {
+		if utils.IsContextDone(ctx) {
+			log.Info("Status update loop is stopping.")
 			return
+		}
+		status, err := r.collectStatus(ctx)
+		if err != nil {
+			log.WithError(err).Warnf("Error collecting system status.")
+			continue
+		}
+
+		if status == nil {
+			continue
+		}
+
+		if err := r.Cache.UpdateStatus(status); err != nil {
+			log.WithError(err).Warnf("Error updating system status in cache.")
+			continue
 		}
 	}
 }
@@ -497,9 +463,12 @@ func (r *agent) statusUpdateLoop() {
 // collectStatus obtains the cluster status by querying statuses of
 // known cluster members.
 func (r *agent) collectStatus(ctx context.Context) (systemStatus *pb.SystemStatus, err error) {
+	ctx, cancel := context.WithTimeout(ctx, statusUpdateTimeout)
+	defer cancel()
+
 	systemStatus = &pb.SystemStatus{
 		Status:    pb.SystemStatus_Unknown,
-		Timestamp: pb.NewTimeToProto(r.statusClock.Now()),
+		Timestamp: pb.NewTimeToProto(r.Clock.Now()),
 	}
 
 	members, err := r.SerfClient.Members()
@@ -516,7 +485,7 @@ func (r *agent) collectStatus(ctx context.Context) (systemStatus *pb.SystemStatu
 
 	statusCh := make(chan *statusResponse, len(members))
 	for _, member := range members {
-		if r.name == member.Name {
+		if r.Name == member.Name {
 			go r.getLocalStatus(ctxNode, member, statusCh)
 		} else {
 			go r.getStatusFrom(ctxNode, member, statusCh)
@@ -551,9 +520,9 @@ L:
 func (r *agent) collectLocalStatus(ctx context.Context, local *serf.Member) (status *pb.NodeStatus) {
 	status = r.runChecks(ctx)
 	status.MemberStatus = statusFromMember(local)
-	r.mu.Lock()
+	r.Lock()
 	r.localStatus = status
-	r.mu.Unlock()
+	r.Unlock()
 
 	return status
 }
@@ -595,7 +564,7 @@ func (r *agent) getStatusFrom(ctx context.Context, member serf.Member, respc cha
 
 // timelineUpdateLoop periodically updates the status timeline.
 func (r *agent) timelineUpdateLoop(ctx context.Context) {
-	ticker := r.timelineClock.NewTicker(statusUpdateTimeout)
+	ticker := r.Clock.NewTicker(statusUpdateTimeout)
 	defer ticker.Stop()
 
 	// index is used select another member of the cluster.
@@ -670,7 +639,7 @@ func (r *agent) selectMember(index int) (member serf.Member, err error) {
 	// Ignore members that have left the cluster.
 	members = filterLeft(members)
 	// Ignore self.
-	members = filterMember(members, r.name)
+	members = filterMember(members, r.Name)
 
 	numAvailable := len(members)
 	if numAvailable < 1 {
@@ -697,7 +666,7 @@ func (r *agent) getTimelineFrom(ctx context.Context, member serf.Member) (events
 
 // recentStatus returns the last known cluster status.
 func (r *agent) recentStatus() (status *pb.SystemStatus, err error) {
-	status, err = r.cache.RecentStatus()
+	status, err = r.Cache.RecentStatus()
 	if err == nil && status == nil {
 		status = pb.EmptyStatus()
 	}
@@ -706,8 +675,8 @@ func (r *agent) recentStatus() (status *pb.SystemStatus, err error) {
 
 // recentLocalStatus returns the last known local node status.
 func (r *agent) recentLocalStatus() *pb.NodeStatus {
-	r.mu.Lock()
-	defer r.mu.Unlock()
+	r.Lock()
+	defer r.Unlock()
 	return r.localStatus
 }
 
@@ -736,6 +705,9 @@ func filterMember(members []serf.Member, name string) (result []serf.Member) {
 	}
 	return result
 }
+
+// DialRPC returns RPC client for the provided Serf member.
+type DialRPC func(context.Context, *serf.Member) (*client, error)
 
 // statusResponse describes a status response from a background process that obtains
 // health status on the specified serf node.
