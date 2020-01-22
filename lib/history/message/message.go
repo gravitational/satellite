4fe051745a9b367731b8e896c1dc98b4133922d5
Bernard Kim <bernard@gravitational.com>
2020-01-22 13:39:23 -0800

    Add message pkg
    - Added Messenger and Subscriber interfaces.
    - Added Queue struct. Implements Messenger and will be used to notify
    cluster members with new local events.
    - Added Receiver struct. Implements Subscriber and will be used to send
    events to cluster members.

---
+++ lib/history/message/message.go
@@ -0,0 +1,44 @@
+/*
+Copyright 2020 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package message provides interfaces for implementing internal communication
+// of timeline messages.
+package message
+
+import (
+	"context"
+	"time"
+
+	pb "github.com/gravitational/satellite/agent/proto/agentpb"
+)
+
+// Subscriber can subscribe to a Messenger.
+type Subscriber interface {
+	// Notifiy notifies the subscriber of new events.
+	Notify(ctx context.Context, events []*pb.TimelineEvent) error
+	// Timestamp returns the last seen timestamp.
+	Timestamp() time.Time
+}
+
+// Messenger notifies subscribers when new events are available.
+type Messenger interface {
+	// Publish an event to the messenger.
+	Publish(ctx context.Context, events []*pb.TimelineEvent) error
+	// Subscribe a new subscriber uniquely identified by the provided id.
+	Subscribe(id string, sub Subscriber) error
+	// Unsubscribe a subscriber specified by the provided id.
+	Unsubscribe(id string) error
+}
---
+++ lib/history/message/receiver.go
@@ -0,0 +1,76 @@
+/*
+Copyright 2020 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package message
+
+import (
+	"context"
+	"time"
+
+	pb "github.com/gravitational/satellite/agent/proto/agentpb"
+	"github.com/gravitational/satellite/lib/client"
+
+	"github.com/gravitational/trace"
+)
+
+// Receiver can be used to subscribe to a Messenger and be notified when new
+// events are available.
+// Implements Subscriber.
+type Receiver struct {
+	// rpcConfig specifies configuration used to create a new rpc client.
+	rpcConfig client.Config
+	// timestamp specifies the last timestamp seen by the receiver.
+	timestamp time.Time
+}
+
+// NewReceiver constructs a new receiver with the provided configuration.
+func NewReceiver(rpcConfig client.Config) *Receiver {
+	return &Receiver{rpcConfig: rpcConfig}
+}
+
+// Timestamp returns the last seen timestamp.
+func (r *Receiver) Timestamp() time.Time {
+	return r.timestamp
+}
+
+// Notify notifies the receiver of new events that need to be sent to the
+// receiving cluster member.
+func (r *Receiver) Notify(ctx context.Context, events []*pb.TimelineEvent) error {
+	return r.pushEvents(ctx, events)
+}
+
+// pushEvents sends the provided events to the receiving cluster member  and
+// updates the last seen timestamp.
+// The provided events must be sorted by timestamp to be able to retry failed
+// updates.
+func (r *Receiver) pushEvents(ctx context.Context, events []*pb.TimelineEvent) error {
+	client, err := client.NewClient(ctx, r.rpcConfig)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	defer client.Close()
+
+	for _, event := range events {
+		if _, err := client.UpdateTimeline(ctx, &pb.UpdateRequest{Event: event}); err != nil {
+			return trace.Wrap(err)
+		}
+		// Subtract a second in case there are multiple events with the same timestamp.
+		// Update timeline might fail on one of these events and needs to be pushed
+		// again.
+		r.timestamp = event.GetTimestamp().ToTime().Add(-time.Second)
+	}
+	return nil
+}
