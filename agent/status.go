238f7d8d8a501d8b571fe63ad452eacc73271e15
dmitri <deemok@gmail.com>
2021-09-02 13:05:39 +0200

    In gravity 7.x, the serf membership code uses the IP.<clustername> as agent names while in 8.x and later tne membership code changed to use the k8s node names.
    In order to keep backwards compatibility with 7.x clusters, use the
    'publicip' tag value as the common denominator. This is only relevant
    for 8.x as long as older clusters are upgraded.

--- agent/status.go
+++ agent/status.go
@@ -57,7 +57,10 @@ func setSystemStatus(status *pb.SystemStatus, members []*pb.MemberStatus) {
 
 	missing := make(memberMap)
 	for _, member := range members {
-		missing[member.Name] = struct{}{}
+		// Instead of indexing by name, use the 'publicip' as the common
+		// denominator to support 7.x clusters
+		addr := member.Tags[publicIPTag]
+		missing[addr] = struct{}{}
 	}
 
 	status.Status = pb.SystemStatus_Running
@@ -71,7 +74,7 @@ func setSystemStatus(status *pb.SystemStatus, members []*pb.MemberStatus) {
 		if node.MemberStatus.Status == pb.MemberStatus_Failed {
 			status.Status = pb.SystemStatus_Degraded
 		}
-		delete(missing, node.Name)
+		delete(missing, node.MemberStatus.Tags[publicIPTag])
 	}
 	if !foundMaster {
 		status.Status = pb.SystemStatus_Degraded
@@ -133,4 +136,7 @@ type memberMap map[string]struct{}
 
 var errNoMaster = errors.New("master node unavailable")
 
-const msgNoStatus = "no status received from nodes (%v)"
+const (
+	msgNoStatus = "no status received from nodes (%v)"
+	publicIPTag = "publicip"
+)
